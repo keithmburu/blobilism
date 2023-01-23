27f9fdbb08523e3789e35c821031e01d21c22880
kmburu <keithmburu1@gmail.com>
2023-01-22 19:29:25 -0500

    keyhole paintbrush, fixed double to int narrowing error

--- src/blobilism.cpp
+++ src/blobilism.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <variant>
 #include <cmath>
 #include "tinygl-cpp.h"
 using namespace tinygl;
@@ -19,21 +18,19 @@ class MyWindow : public Window {
   void setup() override {
     std::cout << "Window size: " << width() << ", " << height() << std::endl;
     for (int i = 0; i < colors.size(); i++) {
-      Circle paletteCirc = {colors[i], 50, 55*(i+0.6), 35};
+      Circle paletteCirc = {colors[i], 50, int(55*(i+0.6)), 35};
       paletteCircles.push_back(paletteCirc);
     }
   }
 
   virtual void mouseMotion(int x, int y, int dx, int dy) {
     if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
-      // todo: store a circle with the current color, size, x, y
       circles.push_back(Circle{currentColor, circleSize, x, y});
     }
   }
   
   virtual void mouseDown(int button, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT) {
-      // todo: check if user clicked a color 
       float mx = mouseX();  // current mouse pos x
       float my = mouseY();  // current mouse pos y
       for (Circle paletteCirc : paletteCircles) {
@@ -41,7 +38,7 @@ class MyWindow : public Window {
         if (dist <= paletteCirc.circleSize/2) {
           currentColor = paletteCirc.color;
           currentColor.push_back(currentAlpha);
-          std::cout << "Changed current color to " << currentColor[0] << " " << currentColor[1] << " " << currentColor[2] << std::endl;
+          std::cout << "Setting color to " << currentColor[0] << " " << currentColor[1] << " " << currentColor[2] << std::endl;
         }
       }
     }
@@ -49,33 +46,28 @@ class MyWindow : public Window {
 
   void keyDown(int key, int mods) {
     if (key == GLFW_KEY_UP) {
-      // increase size of circle
       circleSize += 5;
       std::cout << "Pressed UP: Increase point size to " << circleSize << std::endl;
     } 
     else if (key == GLFW_KEY_DOWN) {
       if (circleSize >= 6) {
-        // decrease size of circle
         circleSize -= 5;
         std::cout << "Pressed DOWN: Decrease point size to " << circleSize << std::endl;
       }
     }
     else if (key == GLFW_KEY_LEFT) {
       if (currentAlpha >= 0.05) {
-        // decrease alpha
         currentAlpha -= 0.05;
         currentColor[3] = currentAlpha;
-        std::cout << "Pressed LEFT: Increase transparency to " << currentAlpha << std::endl;
+        std::cout << "Pressed LEFT: Decrease alpha to " << currentAlpha << std::endl;
       }
     }
     else if (key == GLFW_KEY_RIGHT) {
-      // increase alpha
       currentAlpha += 0.05;
       currentColor[3] = currentAlpha;
-      std::cout << "Pressed RIGHT: Decrease transparency to " << currentAlpha << std::endl;
+      std::cout << "Pressed RIGHT: Increase alpha to " << currentAlpha << std::endl;
     }
     else if (key == GLFW_KEY_C) {
-      // clear vector of circles
       circles.clear();
       std::cout << "Pressed C: Clear canvas" << std::endl;
     }
@@ -84,12 +76,15 @@ class MyWindow : public Window {
   void draw() override {
     background(0.95f, 0.95f, 0.95f); // parameters: r, g, b
 
+    // draw paint
     for (Circle circ : circles) {
       color(circ.color[0], circ.color[1], circ.color[2], circ.color[3]);
+      // keyhole paint brush
       circle(circ.x, circ.y, circ.circleSize);
+      triangle(circ.x, circ.y, circ.circleSize*0.8, circ.circleSize);
     }
 
-    // todo : draw pallet
+    // draw pallet
     color(0.1f, 0.1f, 0.1f);
     square(width()/2.0f, 35, width(), 70);
     for (int i = 0; i < paletteCircles.size(); i++) {
@@ -99,7 +94,6 @@ class MyWindow : public Window {
   }
  private:
 
-  // todo: create member variables for 
   // current circle size
   float circleSize = 5;
   // current transparency
@@ -109,7 +103,7 @@ class MyWindow : public Window {
   // list of circles to draw each frame
   std::vector<Circle> circles;
   // color pallet
-  std::vector<std::vector<float>> colors = {{1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}};
+  std::vector<std::vector<float>> colors = {{1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {1.0f, 0.65f, 0.7f}, {1.0f, 0.65f, 0.0f}, {0.8f, 0.8f, 0.8f}};
   std::vector<Circle> paletteCircles;
 };
 

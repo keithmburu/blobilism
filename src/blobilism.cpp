21e32807bd1046234e215d25977eefabbee1046f
kmburu <keithmburu1@gmail.com>
2023-01-24 06:58:29 -0500

    comments, changed circle to point, added alpha upper limit

--- src/blobilism.cpp
+++ src/blobilism.cpp
@@ -1,7 +1,7 @@
 /**
  * Interactive drawing program
  * 
- * @author your name (you@domain.com)
+ * @author Keith Mburu
  * @date 2023-01-23
  * @file blobilism.cpp
  */
@@ -12,9 +12,9 @@
 #include "tinygl-cpp.h"
 using namespace tinygl;
 
-struct Circle {
+struct Point {
   std::vector<float> color;
-  float circleSize;
+  float pointSize;
   int x;
   int y;
 };
@@ -25,15 +25,17 @@ class MyWindow : public Window {
 
   void setup() override {
     std::cout << "Window size: " << width() << ", " << height() << std::endl;
+    // set up palette
     for (int i = 0; i < colors.size(); i++) {
-      Circle paletteCirc = {colors[i], 50, int(55 * (i + 0.6)), 35};
-      paletteCircles.push_back(paletteCirc);
+      Point palettePoint = {colors[i], 50, int(55 * (i + 0.6)), 35};
+      palettePoints.push_back(palettePoint);
     }
   }
 
   virtual void mouseMotion(int x, int y, int dx, int dy) {
     if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)) {
-      circles.push_back(Circle{currentColor, circleSize, x, y});
+      // draw points
+      points.push_back(Point{currentColor, pointSize, x, y});
     }
   }
   
@@ -41,10 +43,12 @@ class MyWindow : public Window {
     if (button == GLFW_MOUSE_BUTTON_LEFT) {
       float mx = mouseX();  // current mouse pos x
       float my = mouseY();  // current mouse pos y
-      for (Circle paletteCirc : paletteCircles) {
-        float dist = sqrt(pow(mx - paletteCirc.x, 2) + pow(my - paletteCirc.y, 2));
-        if (dist <= paletteCirc.circleSize / 2) {
-          currentColor = paletteCirc.color;
+      for (Point palettePoint : palettePoints) {
+        // distance from pointer to palette color
+        float dist = sqrt(pow(mx - palettePoint.x, 2) + pow(my - palettePoint.y, 2));
+        // change color if user clicked on palette color
+        if (dist <= palettePoint.pointSize / 2) {
+          currentColor = palettePoint.color;
           currentColor.push_back(currentAlpha);
           std::cout << "Setting color to " << currentColor[0] << " " << currentColor[1] << " " << currentColor[2] << std::endl;
         }
@@ -54,13 +58,15 @@ class MyWindow : public Window {
 
   void keyDown(int key, int mods) {
     if (key == GLFW_KEY_UP) {
-      circleSize += 5;
-      std::cout << "Pressed UP: Increase point size to " << circleSize << std::endl;
+      pointSize += 5;
+      std::cout << "Pressed UP: Increase point size to " << pointSize << std::endl;
     } 
     else if (key == GLFW_KEY_DOWN) {
-      if (circleSize >= 6) {
-        circleSize -= 5;
-        std::cout << "Pressed DOWN: Decrease point size to " << circleSize << std::endl;
+      if (pointSize >= 6) {
+        pointSize -= 5;
+        std::cout << "Pressed DOWN: Decrease point size to " << pointSize << std::endl;
+      } else {
+        std::cout << "Pressed DOWN: Point size is already at min value " << pointSize << std::endl;
       }
     }
     else if (key == GLFW_KEY_LEFT) {
@@ -68,15 +74,21 @@ class MyWindow : public Window {
         currentAlpha -= 0.05;
         currentColor[3] = currentAlpha;
         std::cout << "Pressed LEFT: Decrease alpha to " << currentAlpha << std::endl;
+      } else {
+        std::cout << "Pressed LEFT: Alpha is already at min value " << currentAlpha << std::endl;
       }
     }
     else if (key == GLFW_KEY_RIGHT) {
-      currentAlpha += 0.05;
-      currentColor[3] = currentAlpha;
-      std::cout << "Pressed RIGHT: Increase alpha to " << currentAlpha << std::endl;
+      if (currentAlpha <= 0.95) {
+        currentAlpha += 0.05;
+        currentColor[3] = currentAlpha;
+        std::cout << "Pressed RIGHT: Increase alpha to " << currentAlpha << std::endl;
+      } else {
+        std::cout << "Pressed RIGHT: Alpha is already at max value " << currentAlpha << std::endl;
+      }
     }
     else if (key == GLFW_KEY_C) {
-      circles.clear();
+      points.clear();
       std::cout << "Pressed C: Clear canvas" << std::endl;
     }
   }
@@ -84,35 +96,38 @@ class MyWindow : public Window {
   void draw() override {
     background(0.95f, 0.95f, 0.95f); // parameters: r, g, b
 
-    // draw paint
-    for (Circle circ : circles) {
-      color(circ.color[0], circ.color[1], circ.color[2], circ.color[3]);
+    // draw points
+    for (Point point : points) {
+      color(point.color[0], point.color[1], point.color[2], point.color[3]);
       // keyhole paint brush
-      circle(circ.x, circ.y, circ.circleSize);
-      triangle(circ.x, circ.y, circ.circleSize * 0.8, circ.circleSize);
+      circle(point.x, point.y, point.pointSize);
+      triangle(point.x, point.y, point.pointSize * 0.8, point.pointSize);
     }
 
-    // draw pallet
+    // draw palette
     color(0.1f, 0.1f, 0.1f);
+    // background 
     square(width() / 2.0f, 35, width(), 70);
-    for (int i = 0; i < paletteCircles.size(); i++) {
-      color(paletteCircles[i].color[0], paletteCircles[i].color[1], paletteCircles[i].color[2]);
+    // points
+    for (int i = 0; i < palettePoints.size(); i++) {
+      color(palettePoints[i].color[0], palettePoints[i].color[1], palettePoints[i].color[2]);
       circle(55*(i + 0.6), 35, 50);
     }
   }
  private:
 
-  // current circle size
-  float circleSize = 5;
+  // current point size
+  float pointSize = 5;
   // current transparency
   float currentAlpha = 1;
   // current color
   std::vector<float> currentColor = {1.0f, 1.0f, 0.0f, currentAlpha};
-  // list of circles to draw each frame
-  std::vector<Circle> circles;
-  // color pallet
+  // list of points to draw each frame
+  std::vector<Point> points;
+  // palette colors
   std::vector<std::vector<float>> colors = {{1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 1.0f}, {1.0f, 0.65f, 0.7f}, {1.0f, 0.65f, 0.0f}, {0.8f, 0.8f, 0.8f}};
-  std::vector<Circle> paletteCircles;
+  // list of points for each palette color
+  std::vector<Point> palettePoints;
 };
 
 int main() {
